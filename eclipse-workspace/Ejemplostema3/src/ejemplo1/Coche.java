1351e4a611827c7d14c6034847f2410b2f952117
David Díaz Stoica <diaz.stdav24@triana.salesianos.edu>
2025-10-30 11:35:14 +0100

    Tema 3 ejemplos

---
+++ eclipse-workspace/Ejemplostema3/src/ejemplo1/Coche.java
@@ -0,0 +1,14 @@
+package ejemplo1;
+
+public class Coche {
+
+	//Atributos 
+	private String coche;
+	private String modelo; 
+	private String color;
+	private double precioBase; 
+	private int anyo;
+	
+	
+	//Métodos
+}
---
+++ eclipse-workspace/Ejemplostema3/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Ejemplostema3 {
+}
