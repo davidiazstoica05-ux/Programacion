0f067bc5b5d7c080bb5428de2910a2997e377a99
davidiazstoica05-ux <davidiazstoica05@gmail.com>
2025-11-03 22:04:08 +0100

    Ejercicio3

---
+++ eclipse-workspace/EjerciciosTema3DavidDíaz/src/ejercicios3/Operaciones.java
@@ -0,0 +1,30 @@
+package ejercicios3;
+
+public class Operaciones {
+	
+	//Número parOimpar
+
+	public int division(int num1) {
+
+		int num2 = 2;
+
+		return num1 / num2;
+
+	}
+	
+	
+	public boolean parOimpar () {
+		
+		
+	}
+	
+	//Syso para imprimir
+	
+	public void imprimirDatos () {
+		
+		System.out.println("El resultado es "+);
+		
+		
+	}
+
+}
